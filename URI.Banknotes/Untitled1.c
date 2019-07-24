24724e81c8ad4d5955ce0b913244514b283934f5
Julker Nayem <39779149+Nayem95@users.noreply.github.com>
2019-07-23 18:33:30 -0600

    Add files via upload

---
+++ URI.Banknotes/Untitled1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,n,a1,b1,c,c1,d,d1,e,e1,f,f1,g,g1;
+    scanf("%d",&n);
+    printf("%d\n",n);
+    a=n/100;
+    printf("%d nota(s) de R$ 100,00\n",a);
+    a1=n-(a*100);
+    b=a1/50;
+    printf("%d nota(s) de R$ 50,00\n",b);
+    b1=a1-(b*50);
+    c=b1/20;
+    c1=b1-(c*20);
+    printf("%d nota(s) de R$ 20,00\n",c);
+    d=c1/10;
+    d1=c1-(d*10);
+    printf("%d nota(s) de R$ 10,00\n",d);
+    e=d1/5;
+    e1=d1-(e*5);
+    printf("%d nota(s) de R$ 5,00\n",e);
+    f=e1/2;
+    f1=e1-(f*2);
+    printf("%d nota(s) de R$ 2,00\n",f);
+    g=f1/1;
+    g1=f1-(f*1);
+    printf("%d nota(s) de R$ 1,00\n",g);
+
+
+    return 0;
+}
